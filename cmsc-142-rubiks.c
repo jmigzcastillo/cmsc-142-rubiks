46643cc31313b2bec5e949e5164433e7b355edd8
JustinAguinaldo <jstnagnldo@up.edu.ph>
2017-11-14 17:45:39 +0800

    migui

--- cmsc-142-rubiks.c
+++ cmsc-142-rubiks.c
@@ -49,112 +49,132 @@ void rotateCube(char (*c)[][3][3], int color, int orientation) {
     int temp[3][3];
     int temp2[3];
     int temp3[3];
+    int tempR[3], tempB[3], tempO[3], tempG[3];
+    int i,j;
 
-    memcpy(temp, (*c)[color], sizeof(temp));
+    for(i=0;i<3;i++) {
+        for(j=0;j<3;j++)
+            temp[i][j] = (*c)[color][i][j];
+    }
+    
+    for(i=0;i<3;i++) {
+        for(j=0;j<3;j++)
+            temp[i][j] = (*c)[color][i][j];
+    }
+
+    printf("\n\n");
+    for(i=0;i<3;i++) {
+        for(j=0;j<3;j++)
+            printf("%2c",temp[i][j]);
+        printf("\n");
+    }
+    printf("\n\n");
     
     if(orientation) { //CLOCKWISE
         //switch elements inside the 3x3 matrix first
-        for(int i = 0; i < 3; i++) {
-            (*c)[color][i][2] = temp[0][i];
-            (*c)[color][2][i] = temp[i][2];
-            (*c)[color][i][0] = temp[2][i];
-            (*c)[color][0][i] = temp[i][0];
-        }
+        (*c)[color][0][0] = temp[2][0];
+        (*c)[color][0][1] = temp[1][0];
+        (*c)[color][0][2] = temp[0][0];
+        (*c)[color][1][0] = temp[2][1];
+        (*c)[color][1][2] = temp[0][1];
+        (*c)[color][2][0] = temp[2][2];
+        (*c)[color][2][1] = temp[1][2];
+        (*c)[color][2][2] = temp[0][2];
 
-        // for(int j = 0; j < 4; j++) {
-        //     switch(color) {
-        //         case 0 :    //IF WHITE == FACE
-        //             for(int i = 0; i < 3; i++) {
-        //                 //GREEN TO ORANGE
-        //                 temp2[i] = (*c)[3][0][i];
-        //                 (*c)[3][0][i] = (*c)[4][0][i];
-        //                 //ORANGE TO BLUE
-        //                 temp3[i] = (*c)[2][0][i];
-        //                 (*c)[2][0][i] = temp2[i];
-        //                 //BLUE TO RED
-        //                 temp2[i] = (*c)[1][0][i];
-        //                 (*c)[1][0][i] = temp3[i];
-        //                 //RED TO GREEN
-        //                 (*c)[4][0][i] = temp2[i];
-        //             }
-        //             break;
-        //         case 1 :    //IF RED == FACE
-        //             for(int i = 0; i < 3; i++) {
-        //                 //WHITE TO BLUE
-        //                 temp2[i] = (*c)[2][i][0];
-        //                 (*c)[2][i][0] = (*c)[0][i][0];
-        //                 //BLUE TO YELLOW
-        //                 temp3[i] = (*c)[5][i][2];
-        //                 (*c)[5][abs(i-2)][2] = temp2[i];
-        //                 //YELLOW TO GREEN
-        //                 temp2[i] = (*c)[4][i][2];
-        //                 (*c)[4][i][2] = temp3[i];
-        //                 //GREEN TO WHITE
-        //                 (*c)[0][abs(i-2)][0] = temp2[i];
-        //             }
-        //             break;
-        //         case 2 :    //IF BLUE == FACE
-        //             for(int i = 0; i < 3; i++) {
-        //                 //WHITE TO ORANGE
-        //                 temp2[i] = (*c)[3][i][0];
-        //                 (*c)[3][i][0] = (*c)[0][2][i];
-        //                 //ORANGE TO YELLOW
-        //                 temp3[i] = (*c)[5][2][i];
-        //                 (*c)[5][2][i] = temp2[i];
-        //                 //YELLOW TO RED
-        //                 temp2[i] = (*c)[1][i][2];
-        //                 (*c)[1][abs(i-2)][2] = temp3[i];
-        //                 //RED TO WHITE
-        //                 (*c)[0][2][abs(i-2)] = temp2[i];
-        //             }
-        //             break;
-        //         case 3 :    //IF ORANGE == FACE
-        //             for(int i = 0; i < 3; i++) {
-        //                 //WHITE TO GREEN
-        //                 temp2[i] = (*c)[4][i][0];
-        //                 (*c)[4][i][0] = (*c)[0][abs(i-2)][2];
-        //                 //GREEN TO YELLOW
-        //                 temp3[i] = (*c)[5][i][0];
-        //                 (*c)[5][i][0] = temp2[i];
-        //                 //YELLOW TO BLUE
-        //                 temp2[i] = (*c)[2][i][2];
-        //                 (*c)[2][abs(i-2)][2] = temp3[i];
-        //                 //BLUE TO WHITE
-        //                 (*c)[0][i][2] = temp2[i];
-        //             }
-        //             break;
-        //         case 4 :    //IF GREEN == FACE
-        //             for(int i = 0; i < 3; i++) {
-        //                 //WHITE TO RED
-        //                 temp2[i] = (*c)[1][i][0];
-        //                 (*c)[1][i][0] = (*c)[0][0][abs(i-2)];
-        //                 //RED TO YELLOW
-        //                 temp3[i] = (*c)[5][i][0];
-        //                 (*c)[5][abs(i-2)][0] = temp2[i];
-        //                 //YELLOW TO ORANGE
-        //                 temp2[i] = (*c)[2][i][2];
-        //                 (*c)[2][i][2] = temp3[i];
-        //                 //ORANGE TO WHITE
-        //                 (*c)[0][i][2] = temp2[i];
-        //             }
-        //             break;
-        //         case 5 :    //IF YELLOW == FACE
-        //             for(int i = 0; i < 3; i++) {
-        //                 //GREEN TO RED
-        //                 temp2[i] = (*c)[1][2][i];
-        //                 (*c)[1][2][i] = (*c)[4][2][i];
-        //                 //RED TO BLUE
-        //                 temp3[i] = (*c)[2][2][i];
-        //                 (*c)[2][2][i] = temp2[i];
-        //                 //BLUE TO ORANGE
-        //                 temp2[i] = (*c)[3][2][i];
-        //                 (*c)[3][2][i] = temp3[i];
-        //                 //ORANGE TO GREEN
-        //                 (*c)[4][2][i] = temp2[i];
-        //             }
-        //             break;
-        //     }
-        // }
+        for(int j = 0; j < 4; j++) {
+            switch(color) {
+                case 0 :    //IF WHITE == FACE
+                    for(int i = 0; i < 3; i++) {
+                        //GREEN TO ORANGE
+                        temp2[i] = (*c)[3][0][i];
+                        (*c)[3][0][i] = (*c)[4][0][i];
+                        //ORANGE TO BLUE
+                        temp3[i] = (*c)[2][0][i];
+                        (*c)[2][0][i] = temp2[i];
+                        //BLUE TO RED
+                        temp2[i] = (*c)[1][0][i];
+                        (*c)[1][0][i] = temp3[i];
+                        //RED TO GREEN
+                        (*c)[4][0][i] = temp2[i];
+                    }
+                    break;
+                case 1 :    //IF RED == FACE
+                    for(int i = 0; i < 3; i++) {
+                        //WHITE TO BLUE
+                        temp2[i] = (*c)[2][i][0];
+                        (*c)[2][i][0] = (*c)[0][i][0];
+                        //BLUE TO YELLOW
+                        temp3[i] = (*c)[5][i][2];
+                        (*c)[5][abs(i-2)][2] = temp2[i];
+                        //YELLOW TO GREEN
+                        temp2[i] = (*c)[4][i][2];
+                        (*c)[4][i][2] = temp3[i];
+                        //GREEN TO WHITE
+                        (*c)[0][abs(i-2)][0] = temp2[i];
+                    }
+                    break;
+                case 2 :    //IF BLUE == FACE
+                    for(int i = 0; i < 3; i++) {
+                        //WHITE TO ORANGE
+                        temp2[i] = (*c)[3][i][0];
+                        (*c)[3][i][0] = (*c)[0][2][i];
+                        //ORANGE TO YELLOW
+                        temp3[i] = (*c)[5][2][i];
+                        (*c)[5][2][i] = temp2[i];
+                        //YELLOW TO RED
+                        temp2[i] = (*c)[1][i][2];
+                        (*c)[1][abs(i-2)][2] = temp3[i];
+                        //RED TO WHITE
+                        (*c)[0][2][abs(i-2)] = temp2[i];
+                    }
+                    break;
+                case 3 :    //IF ORANGE == FACE
+                    for(int i = 0; i < 3; i++) {
+                        //WHITE TO GREEN
+                        temp2[i] = (*c)[4][i][0];
+                        (*c)[4][i][0] = (*c)[0][abs(i-2)][2];
+                        //GREEN TO YELLOW
+                        temp3[i] = (*c)[5][i][0];
+                        (*c)[5][i][0] = temp2[i];
+                        //YELLOW TO BLUE
+                        temp2[i] = (*c)[2][i][2];
+                        (*c)[2][abs(i-2)][2] = temp3[i];
+                        //BLUE TO WHITE
+                        (*c)[0][i][2] = temp2[i];
+                    }
+                    break;
+                case 4 :    //IF GREEN == FACE
+                    for(int i = 0; i < 3; i++) {
+                        //WHITE TO RED
+                        temp2[i] = (*c)[1][i][0];
+                        (*c)[1][i][0] = (*c)[0][0][abs(i-2)];
+                        //RED TO YELLOW
+                        temp3[i] = (*c)[5][i][0];
+                        (*c)[5][abs(i-2)][0] = temp2[i];
+                        //YELLOW TO ORANGE
+                        temp2[i] = (*c)[2][i][2];
+                        (*c)[2][i][2] = temp3[i];
+                        //ORANGE TO WHITE
+                        (*c)[0][i][2] = temp2[i];
+                    }
+                    break;
+                case 5 :    //IF YELLOW == FACE
+                    for(int i = 0; i < 3; i++) {
+                        //GREEN TO RED
+                        temp2[i] = (*c)[1][2][i];
+                        (*c)[1][2][i] = (*c)[4][2][i];
+                        //RED TO BLUE
+                        temp3[i] = (*c)[2][2][i];
+                        (*c)[2][2][i] = temp2[i];
+                        //BLUE TO ORANGE
+                        temp2[i] = (*c)[3][2][i];
+                        (*c)[3][2][i] = temp3[i];
+                        //ORANGE TO GREEN
+                        (*c)[4][2][i] = temp2[i];
+                    }
+                    break;
+            }
+        }
     } else {
     }
 }
